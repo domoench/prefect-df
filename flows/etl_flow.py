0e15bc0622d75e8e27e432afb6bbb6f5f3796fd1
David Ouyang Moench <domoench@gmail.com>
2024-07-03 11:44:19 -0400

    Use flow.from_source(...).deploy(...).
    - Switched to a Process-type work pool instead of a Docker-type. This is
      because my worker is already running in a container, so the
      Docker-type work pool was launching another container inside the
      container, and the 2nd layer container did not have the flows copied
      into its filesystem.
    - Uses the flow.from_source(...).deploy(...) pattern to specify the
      local flow source file location, according to this recent Prefect
      update:
      https://github.com/PrefectHQ/prefect/pull/13981

--- flows/etl_flow.py
+++ flows/etl_flow.py
@@ -9,27 +9,15 @@ def etl():
 
 if __name__ == "__main__":
 
-# I'm attempting to deploy my flow and have it run on a local worker with
-# the flow stored in that local workers file system.
-
-# The commented out approach below doesn't work, the worker successfully
-# notices the deployment run, but errors with:
-# FileNotFoundError: [Errno 2] No such file or directory: '/opt/prefect/flows/etl_flow.py'
-# Even though I confirmed that file does indeed exist.
-#    etl.deploy(
-#        name="etl",
-#        work_pool_name="lf-dev",
-#        image="docker.io/prefecthq/prefect:2-python3.10",
-#        build=False,
-#    )
-
-# I thought perhaps it was because the path/endpoint was not properly defined somehow.
-# So I tried the more manual approach below, but no change.
-    deployment = Deployment.build_from_flow(
-        flow=etl,
+    # For dev workers, deploy to Process-type dev work pool.
+    # Using flow.deploy (per https://github.com/PrefectHQ/prefect/pull/13981), despite
+    # reccomendations that flow.serve is better for development, on the hope that
+    # it will be easier to use the same logic for both dev and prod. However I'm probably
+    # wrong about that and just don't know it yet.
+    etl.from_source(
+        source="/opt/prefect/flows/",
+        entrypoint="etl_flow.py:etl",
+    ).deploy(
         name="etl",
         work_pool_name="lf-dev",
-        path="/opt/prefect/",
-        entrypoint="flows/etl_flow.py:etl"
     )
-    deployment.apply()
