6ea9b38498b3e42dbfb55ae64576e3156b0e6027
David Ouyang Moench <domoench@gmail.com>
2024-07-03 14:28:09 -0400

    Install flake8 in dev packages.

--- flows/etl_flow.py
+++ flows/etl_flow.py
@@ -1,5 +1,5 @@
 from prefect import flow
-from prefect.deployments import Deployment
+
 
 @flow(log_prints=True)
 def etl():
@@ -7,13 +7,14 @@ def etl():
     print("Transforming timeseries.")
     print("Loading timeseries into warehouse.")
 
+
 if __name__ == "__main__":
 
     # For dev workers, deploy to Process-type dev work pool.
-    # Using flow.deploy (per https://github.com/PrefectHQ/prefect/pull/13981), despite
-    # reccomendations that flow.serve is better for development, on the hope that
-    # it will be easier to use the same logic for both dev and prod. However I'm probably
-    # wrong about that and just don't know it yet.
+    # Using flow.deploy (per https://github.com/PrefectHQ/prefect/pull/13982),
+    # despite reccomendations that flow.serve is better for development, on
+    # the hope that it will be easier to use the same logic for both dev and
+    # prod. However I'm probably wrong about that and just don't know it yet.
     etl.from_source(
         source="/opt/prefect/flows/",
         entrypoint="etl_flow.py:etl",
