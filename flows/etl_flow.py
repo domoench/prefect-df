9afdc30b81d4c9178ee86be808c6c7e0726f04f1
David Ouyang Moench <domoench@gmail.com>
2024-07-10 11:46:25 -0400

    Fetch EIA API data.
    Also generate the worker's requirements.txt file from the repository's
    Pipfile.lock via `pipenv_to_requirements -f`. Later it might be worth
    automating: Issue #1.

--- flows/etl_flow.py
+++ flows/etl_flow.py
@@ -1,5 +1,8 @@
 from prefect import flow, task
 from utils.storage import get_s3_client
+import requests
+import pandas as pd
+import os
 
 
 @task
@@ -8,10 +11,62 @@ def extract():
     # TODO: Fetch EIA data
     # TODO: Demonstrate concurrent fetch of paginated API requests
 
+    url = ('https://api.eia.gov/v2/electricity/rto/region-data/data/?'
+           'frequency=hourly&data[0]=value&facets[respondent][]=PJM&'
+           'facets[type][]=D&facets[type][]=DF&sort[0][column]=period&'
+           'sort[0][direction]=asc')
+
+    # Calculate the number of rows to fetch from the API between start and end
+    start = pd.to_datetime('2015-07-01 05:00:00+00:00')
+    end = pd.to_datetime('2024-06-28 04:00:00+00:00')
+    time_span = end - start
+    hours = int(time_span.total_seconds() / 3600)
+
+    # Calculate how many paginated API requests will be required to fetch all
+    # the timeseries data
+    REQUEST_ROWS = 5000
+    num_full_requests = hours // REQUEST_ROWS
+    final_request_length = hours % REQUEST_ROWS
+    print(f'Fetching {hours} hours of data. Start: {start}. End: {end}')
+    print((f'Will make {num_full_requests} {REQUEST_ROWS}-length requests and '
+           f'one {final_request_length}-length request.'))
+
+    # Build a list of dataframes storing each API request (page)'s response
+    response_dfs = []
+
+    def append_EIA_page_response_df(start, end, offset, length, result_list):
+        print(f'Fetching API page. offset:{offset}. length:{length}')
+
+        params = {
+            'offset': offset,
+            'length': length,
+            'api_key': os.environ['EIA_API_KEY'],
+            'start': start.strftime('%Y-%m-%dT%H'),
+            'end': end.strftime('%Y-%m-%dT%H'),
+        }
+
+        r = requests.get(url, params=params)
+        r.raise_for_status()
+        result_df = pd.DataFrame(r.json()['response']['data'])
+        result_list.append(result_df)
+
+    # Make the full-length requests
+    for i in range(num_full_requests):
+        offset = i * REQUEST_ROWS
+        append_EIA_page_response_df(start, end, offset, REQUEST_ROWS,
+                                    response_dfs)
+    # Make the final remainder request
+    append_EIA_page_response_df(start, end, num_full_requests * REQUEST_ROWS,
+                                final_request_length, response_dfs)
+
+    api_df = pd.concat(response_dfs)
+    return api_df
+
 
 @task
-def transform():
-    print("Transforming timeseries.")
+def transform(df):
+    print('Transforming timeseries.')
+    print(f'Dataframe length: {len(df)}')
     # Save into a dataframe
     # Convert types
     # Imputation? Or save that for later down the pipe?
@@ -33,6 +88,6 @@ def etl():
     """Pulls all available hourly EIA demand data up to the last XXXday and
     persists it in the S3 data warehouse as a parquet file.
     """
-    extract()
-    transform()
+    df = extract()
+    transform(df)
     load()
