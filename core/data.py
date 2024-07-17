9280abe4c387b72f9e38942dcc49b1db6d11b1a9
David Ouyang Moench <domoench@gmail.com>
2024-07-17 19:11:59 -0400

    Forecasting exploration: Outlier capping and null imputation.

--- core/data.py
+++ core/data.py
@@ -4,13 +4,15 @@ Module containing logic for getting and persisting data, and feature engineering
 
 
 def add_temporal_features(df):
-    """Given a DataFrame with a datetime column 'utc_ts', return a copy with
+    """Given a DataFrame with a datetime index, return a copy with
     added temporal feature columns.
     """
     df = df.copy()
-    df['month'] = df.index.month
     df['hour'] = df.index.hour
+    df['month'] = df.index.month
+    df['year'] = df.index.year
     df['quarter'] = df.index.quarter
-    df['dayofyear'] = df.index.dayofyear
+    df['dayofweek'] = df.index.dayofweek
     df['dayofmonth'] = df.index.day
+    df['dayofyear'] = df.index.dayofyear
     return df
