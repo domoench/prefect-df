9acf8f6bfb8e970d1218ab78a82f1e430135b5b9
David Ouyang Moench <domoench@gmail.com>
2024-07-19 14:51:23 -0400

    Enable logging in jupyter and non-jupyter contexts.
    - In jupyter, log to std out with no format string (just like print)
    - In non-jupyter, do the same. But need to override Prefect's logging
      handler (which writes to stderr stream) to see the output in the
      worker stdout.
    - Accept the fact that only my logger output (from core pacakge) will
      not be visible in the prefect UI. For that need to use print
      statements or prefect logger in flow/task code.

--- core/logging.py
+++ core/logging.py
@@ -1,26 +1,32 @@
-from prefect import get_run_logger
-from prefect.exceptions import MissingContextError
+import logging
+import sys
+from IPython import get_ipython
 
 
-class Logger:
-    """Rudimentary logger that logs to prefect if in a prefect context,
-    and to print statements if not (e.g. in a notebook)"""
-    def __init__(self):
-        try:
-            self.logger = get_run_logger()
-        except MissingContextError:
-            print('Not logging in Prefect context')
-            self.logger = None
-
-    def info(self, s):
-        if self.logger:
-            self.logger.info(s)
-        else:
-            print(s)
+def in_jupyter():
+    try:
+        if 'IPKernelApp' in get_ipython().config:
+            return True
+    except AttributeError:
+        pass
+    return False
 
 
-# Singleton logger instance
-logger = Logger()
+# Singleton logger
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+# Configure handlers
+stream_handler = logging.StreamHandler(sys.stdout)
+stream_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(message)s')
+stream_handler.setFormatter(formatter)
+if not in_jupyter():
+    # In the prefect context they add their own handler to stderr.
+    # Remove that so I can see log lines from my core module
+    for handler in logger.handlers:
+        logger.removeHandler(handler)
+logger.addHandler(stream_handler)
 
 
 def get_logger():
