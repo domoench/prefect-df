4b775985fb9204ae928f99593a6c5512faa8ab1e
David Ouyang Moench <domoench@gmail.com>
2024-09-12 15:42:54 -0400

    Fix missing backslash.
    Which ended the command, leading the mlflow server to use its default
    port.

--- services/mlflow/entrypoint.sh
+++ services/mlflow/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mlflow server \
-  --host 0.0.0.0
+  --host 0.0.0.0 \
   --port ${MLFLOW_TRACKING_PORT} \
   --backend-store-uri ${MLFLOW_BACKEND_STORE_URI} \
   --artifacts-destination ${MLFLOW_ARTIFACTS_DESTINATION}
