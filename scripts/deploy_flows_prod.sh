f46f7956fe500fd8da0692916d750a6c2add0237
David Ouyang Moench <domoench@gmail.com>
2024-10-03 15:40:33 -0400

    Add flows and their deployment configs.

--- scripts/deploy_flows_prod.sh
+++ scripts/deploy_flows_prod.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# Build and push python dependencies base image
+echo "Building and pushing python dependencies base image."
 docker compose build flow-deps
 docker push ${AWS_ECR_ENDPOINT}/prefect-df/flows/flow-deps:latest
 
-# Build flow execution images
-prefect deploy --name test_flow --prefect-file flows/deployments/test_flow.yaml
-prefect deploy --name etl_flow --prefect-file flows/deployments/etl_flow.yaml
+# Build flow execution images and push to ECR
+flowNames=('etl_flow' 'train_model_flow' 'etl_and_train_flow')
+for flowName in "${flowNames[@]}"; do
+  echo "Deploying flow: $flowName"
+  prefect deploy --name $flowName --prefect-file "flows/deployments/${flowName}.yaml"
+done
