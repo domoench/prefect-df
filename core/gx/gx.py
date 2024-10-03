7bd24a1f8e228af13da4ee28e320572acb2cf61f
David Ouyang Moench <domoench@gmail.com>
2024-10-03 15:37:20 -0400

    Update great expectations code for v1.0.6
    Upgraded the gx package, but never updated the code to comply with
    breaking changes.

--- core/gx/gx.py
+++ core/gx/gx.py
@@ -18,37 +18,38 @@ def get_gx_context():
 def initialize_gx():
     """Set up the great expectations context and expectation suites"""
     gx_ctx = gx.get_context()
-    gx_ctx.sources.add_pandas(name=GX_DATASOURCE_NAME)
+    gx_ctx.data_sources.add_pandas(name=GX_DATASOURCE_NAME)
 
     for s in EXPECTATION_SUITES:
-        suite = gx_ctx.add_expectation_suite(expectation_suite_name=s['name'])
-        for exp_cfg in s['exp_cfgs']:
-            suite.add_expectation(expectation_configuration=exp_cfg)
-        gx_ctx.save_expectation_suite(expectation_suite=suite)
+        # suite = gx_ctx.suites.add(name=s['name'])
+        suite = gx.ExpectationSuite(name=s['name'])
+        gx_ctx.suites.add(suite)
+        for expectation in s['expectations']:
+            suite.add_expectation(expectation)
 
     return gx_ctx
 
 
+# TODO rename to remove the word checkpoint
 def run_gx_checkpoint(suite_name, df):
     gx_ctx = get_gx_context()
-    datasource = gx_ctx.get_datasource(GX_DATASOURCE_NAME)
-    data_asset = datasource.add_dataframe_asset(name=f'{suite_name}-df')
-    batch_request = data_asset.build_batch_request(dataframe=df)
-    checkpoint_name = f'{suite_name}-checkpoint'
-    checkpoint = gx_ctx.add_or_update_checkpoint(
-        name=checkpoint_name,
-        validations=[
-            {
-                'batch_request': batch_request,
-                'expectation_suite_name': suite_name,
-            },
-        ],
+
+    # Connect Data
+    data_source = gx_ctx.get_datasource(name=GX_DATASOURCE_NAME)
+    df_asset_name = f'{suite_name}-df'
+    data_asset = data_source.add_dataframe_asset(name=df_asset_name)
+    batch_definition = data_asset.add_batch_definition_whole_dataframe(f'{df_asset_name}-batch_def')
+
+    # Create Validation Definition
+    suite = gx_ctx.suites.get(name=suite_name)
+    validation_definition = gx.ValidationDefinition(
+        data=batch_definition, suite=suite, name=f'{suite_name}-val'
     )
-    result = checkpoint.run(result_format='BASIC')
-    if not result['success']:
+    results = validation_definition.run(batch_parameters={'dataframe': df})
+    if not results['success']:
         # TODO: Something more robust. Log event to datadog for monitoring?
         # Generate data doc artifact?
-        print(f'GX Checkpoint failure: checkpoint_name:{checkpoint_name}')
-        print(result.list_validation_results())
+        print(f'GX Validation failure. suite:{suite_name}')
+        print(results.describe_dict())
     else:
-        print(f'GX Checkpoint success: checkpoint_name:{checkpoint_name}')
+        print(f'GX Validation success: suite:{suite_name}')
--- core/gx/suites.py
+++ core/gx/suites.py
@@ -1,61 +1,54 @@
 from core.consts import EIA_MAX_D_VAL, EIA_MIN_D_VAL, TIME_FEATURES, TARGET
-from great_expectations.expectations.expectation_configuration import (
-    ExpectationConfiguration,
-)
+import great_expectations as gx
 
 # Import custom expectations to ensure they are registered
 from core.gx.custom.expect_column_values_on_the_hour import ExpectColumnValuesOnTheHour
 
-
 EXPECTATION_SUITES = [
-    # Expectations on the dataframe pulled from the ETL warehouse
     {
+        # Expectations on the dataframe pulled from the ETL warehouse
         'name': 'etl',
-        'exp_cfgs': [
-            ExpectationConfiguration(
-                type='expect_column_to_exist',
-                kwargs={'column': 'D'},
+        'expectations': [
+            gx.expectations.ExpectColumnToExist(
+                column='D',
             ),
-            ExpectationConfiguration(
-                type='expect_column_values_to_not_be_null',
-                kwargs={'column': 'D', 'mostly': 0.9},
+            gx.expectations.ExpectColumnValuesToNotBeNull(
+                column='D',
+                mostly=0.9,
             ),
-            ExpectationConfiguration(
-                type='expect_column_values_on_the_hour',
-                kwargs={'column': 'utc_ts', 'mostly': 1.0},
+            ExpectColumnValuesOnTheHour(
+                column='utc_ts',
+                mostly=1.0,
             ),
-        ]
+        ],
     },
 
     # Expectations on the pre-processed dataframe (that will serve as training
     # data to the xgboost model).
     {
         'name': 'train',
-        'exp_cfgs': [
-            ExpectationConfiguration(
-                type='expect_table_columns_to_match_set',
-                kwargs={
-                    'column_set': ['utc_ts'] + TIME_FEATURES + [TARGET],
-                    'exact_match': False,
-                }
+        'expectations': [
+            gx.expectations.ExpectTableColumnsToMatchSet(
+                column_set=['utc_ts'] + TIME_FEATURES + [TARGET],
+                exact_match=False,
             ),
-            ExpectationConfiguration(
-                type='expect_column_values_to_not_be_null',
-                kwargs={'column': 'D', 'mostly': 1.0},
+            gx.expectations.ExpectColumnValuesToNotBeNull(
+                column='D',
+                mostly=1.0,
             ),
-            ExpectationConfiguration(
-                type='expect_column_values_to_be_between',
-                kwargs={'column': 'D', 'min_value': EIA_MIN_D_VAL, 'max_value': EIA_MAX_D_VAL},
+            gx.expectations.ExpectColumnValuesToBeBetween(
+                column='D',
+                min_value=EIA_MIN_D_VAL,
+                max_value=EIA_MAX_D_VAL,
             ),
-            ExpectationConfiguration(
-                type='expect_column_values_to_be_unique',
-                kwargs={'column': 'utc_ts'},
+            gx.expectations.ExpectColumnValuesToBeUnique(
+                column='utc_ts',
             ),
-            ExpectationConfiguration(
-                type='expect_column_values_on_the_hour',
-                kwargs={'column': 'utc_ts', 'mostly': 1.0},
+            ExpectColumnValuesOnTheHour(
+                column='utc_ts',
+                mostly=1.0,
             ),
             # TODO validate the number of entries based on the start+end timestamps
-        ]
-    },
+        ],
+    }
 ]
