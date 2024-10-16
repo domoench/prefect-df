cf4cdab6e55e5182ea10ffe5cb201ac3d92bb6d9
David Ouyang Moench <domoench@gmail.com>
2024-10-16 12:57:13 -0400

    Add compare_model_flows to deployment script.

--- scripts/deploy_flows_prod.sh
+++ scripts/deploy_flows_prod.sh
@@ -5,7 +5,7 @@ docker compose build flow-deps
 docker push ${AWS_ECR_ENDPOINT}/prefect-df/flows/flow-deps:latest
 
 # Build flow execution images and push to ECR
-flowNames=('etl_flow' 'train_model_flow' 'etl_and_train_flow')
+flowNames=('etl_flow' 'train_model_flow' 'etl_and_train_flow' 'compare_models_flow')
 for flowName in "${flowNames[@]}"; do
   echo "Deploying flow: $flowName"
   prefect deploy --name $flowName --prefect-file "flows/deployments/${flowName}.yaml"
